781a10ce6c02e2a94c988e13dd3564c9a7668bf7
David Ross <david@djr.com>
2017-09-30 21:17:08 -0400

    light and dark font, let's see

--- scripts/makeFeatureFont.py
+++ scripts/makeFeatureFont.py
@@ -2,13 +2,6 @@ from lib.UI.spaceCenter.glyphSequenceEditText import splitText
 from fbits.toolbox.font import FontTX
 tags = ['pnum', 'onum', 'tnum', 'lnum', 'liga', 'dlig', 'hlig', 'case', 'cpsp', 'smcp', 'c2sc', 'sups', 'sinf', 'subs', 'ordn', 'titl', 'swsh', 'cswh', 'aalt', 'calt', 'clig', 'frac', 'afrc', 'numr', 'dnom', 'ornm', 'salt', 'ss01', 'hist', 'zero', 'jalt', 'nalt', 'mgrk', 'pcap', 'c2pc', 'unic', 'ccmp', 'rlig', 'mark', 'mkmk', 'mset', 'fina', 'init', 'isol', 'medi']
 
-padding = 100
-
-sourceList = list('abcdefghijklmnopqrstuvwxyz')+['space', 'zero', 'one', 'two', 'three', 'four', 'five' ,'six', 'seven', 'eight', 'nine', 'braceleft', 'braceright']
-substitutions = list('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
-
-
-
 src = CurrentFont()
 f = NewFont()
 
@@ -18,7 +11,7 @@ for gname in sourceList:
 for i, tag in enumerate(tags):
     gnames = splitText(tag, src.getCharacterMapping())
     sub = substitutions[i]
-    slugContents = ['braceleft'] + gnames + ['braceright']
+    slugContents = [gname+'.t' for gname in gnames]
     gname = 'tag_'+tag
     FontTX.setSlug(f, gname, slugContents, decompose=True)
     g = f[gname]
